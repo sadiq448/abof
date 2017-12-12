3203a8ea83d13a50e05a4f607071fcf472500d7b
sadiq <sadiq448@gmail.com>
2017-12-12 11:06:32 +0530

    files are added

---
+++ src/main/java/com/abof/App.java
@@ -0,0 +1,13 @@
+package com.abof;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
